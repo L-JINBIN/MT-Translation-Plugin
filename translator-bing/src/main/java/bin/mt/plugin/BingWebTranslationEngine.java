6c38d3904adfe864fd6fbe011c69846d79de7183
L-JINBIN <921558445@qq.com>
2022-10-05 20:49:33 +0800

    增加必应翻译

---
+++ translator-bing/src/main/java/bin/mt/plugin/BingWebTranslationEngine.java
@@ -0,0 +1,62 @@
+package bin.mt.plugin;
+
+import android.support.annotation.NonNull;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import bin.mt.plugin.api.LocalString;
+import bin.mt.plugin.api.translation.BaseTranslationEngine;
+
+/**
+ * 谷歌翻译Web版
+ *
+ * @author Bin
+ */
+public class BingWebTranslationEngine extends BaseTranslationEngine {
+    private final List<String> sourceLanguages = Arrays.asList("auto",
+            "zh-CN", "zh-TW", "en", "da", "uk", "uz", "ur", "hy", "ru", "bg", "hr", "is",
+            "ca", "hu", "af", "kn", "hi", "id", "gu", "kk", "tr", "cy", "bn", "ne", "iw",
+            "el", "ku", "de", "it", "lv", "cs", "sk", "sl", "sw", "pa", "ja", "ps", "ka",
+            "mi", "fr", "pl", "bs", "fa", "te", "ta", "th", "ht", "ga", "et", "sv", "zu",
+            "lt", "ug", "my", "ro", "lo", "fi", "hmn", "nl", "tl", "sm", "pt", "es", "vi",
+            "az", "am", "sq", "ar", "ko", "mk", "mr", "ml", "ms", "mt", "km", "so");
+
+    private final List<String> targetLanguages = sourceLanguages.subList(1, sourceLanguages.size());
+
+    private LocalString string;
+
+    @Override
+    protected void init() {
+        string = getContext().getAssetLocalString("String");
+    }
+
+    @NonNull
+    @Override
+    public String name() {
+        return string.get("plugin_name");
+    }
+
+    @NonNull
+    @Override
+    public List<String> loadSourceLanguages() {
+        return sourceLanguages;
+    }
+
+    @NonNull
+    @Override
+    public List<String> loadTargetLanguages(String sourceLanguage) {
+        return targetLanguages;
+    }
+
+    @NonNull
+    @Override
+    public String translate(String text, String sourceLanguage, String targetLanguage) throws IOException {
+        return BingWebTranslator.translate(text, sourceLanguage, targetLanguage);
+    }
+
+    @Override
+    public void onFinish() {
+    }
+}
---
+++ translator-bing/src/main/java/bin/mt/plugin/BingWebTranslator.java
@@ -0,0 +1,119 @@
+package bin.mt.plugin;
+
+import org.json2.JSONArray;
+import org.json2.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Bin
+ */
+public class BingWebTranslator {
+    private static final Map<String, String> LANG_MAP = new HashMap<>();
+    private static String ig;
+    private static String iid;
+    private static String token;
+    private static String key;
+
+    static {
+        LANG_MAP.put("auto", "auto-detect");
+        LANG_MAP.put("zh-CN", "zh-Hans");
+        LANG_MAP.put("zh-TW", "zh-Hant");
+        LANG_MAP.put("iw", "he");
+        LANG_MAP.put("hmn", "mww");
+        LANG_MAP.put("tl", "fil");
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(translate("apple", "en", "zh-Hans"));
+    }
+
+    public static String translate(String query, String from, String to) throws IOException {
+        if (LANG_MAP.containsKey(from)) {
+            from = LANG_MAP.get(from);
+        }
+        if (LANG_MAP.containsKey(to)) {
+            to = LANG_MAP.get(to);
+        }
+        for (int i = 0; i < 2; i++) {
+            if (ig == null) {
+                init();
+            }
+            String result = translateImpl(query, from, to);
+            if (result.contains("statusCode")) {
+                processError(result); // 出现205错误时才会进行下一个循环
+            } else {
+                return getResult(result);
+            }
+        }
+        throw new IOException("Translation failed");
+    }
+
+    private static String translateImpl(String query, String from, String to) throws IOException {
+        return HttpUtils.post("https://cn.bing.com/ttranslatev3?IG=" + ig + "&IID=" + iid + ".1")
+                .formData("fromLang", from)
+                .formData("text", query)
+                .formData("to", to)
+                .formData("token", token)
+                .formData("key", key)
+                .execute();
+    }
+
+    private static String getResult(String result) {
+        StringBuilder sb = new StringBuilder();
+        JSONArray array = new JSONArray(result);
+        for (int i = 0; i < array.length(); i++) {
+            JSONArray translations = array.getJSONObject(i).getJSONArray("translations");
+            for (int j = 0; j < translations.length(); j++) {
+                sb.append(translations.getJSONObject(j).getString("text"));
+            }
+        }
+        return sb.toString();
+    }
+
+    private static void processError(String result) throws IOException {
+        JSONObject json = new JSONObject(result);
+        int statusCode = json.getInt("statusCode");
+        String errorMessage = json.getString("errorMessage");
+        if (statusCode != 205) {
+            throw new IOException(errorMessage.isEmpty() ? "StatusCode: " + statusCode : errorMessage);
+        }
+        ig = null;
+    }
+
+    private static void init() throws IOException {
+        String src = HttpUtils.get("https://cn.bing.com/translator").execute();
+        ig = getMiddleText(src, "IG:\"", "\"");
+        iid = getMiddleText(src, "<div id=\"rich_tta\" data-iid=\"", "\"");
+        String richTranslateHelper = getMiddleText(src, "params_RichTranslateHelper = ", ";");
+        if (ig == null) {
+            throw new IOException("Can not parse parameter: ig");
+        }
+        if (iid == null) {
+            throw new IOException("Can not parse parameter: iid");
+        }
+        if (richTranslateHelper == null) {
+            throw new IOException("Can not parse parameter: richTranslateHelper");
+        }
+        JSONArray array = new JSONArray(richTranslateHelper);
+        token = array.getString(1);
+        key = String.valueOf(array.getLong(0));
+    }
+
+    private static String getMiddleText(String str, String start, String end) {
+        int i = str.indexOf(start);
+        if (i == -1) {
+            return null;
+        }
+        i += start.length();
+        int j = str.indexOf(end, i);
+        if (j == -1) {
+            return null;
+        }
+        return str.substring(i, j);
+    }
+
+
+}
